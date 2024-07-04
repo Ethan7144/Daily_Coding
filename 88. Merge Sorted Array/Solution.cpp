44bf406651aaeea1fbdf167769b0c9a669dc5e84
Ethan Tarlov <ejtarlov@me.com>
2024-07-04 10:28:31 -0700

    C++ Solution
            Submission Statistics:
            Question #: 88
            Language: C++
            Runtime: 5 ms
            Runtime percentile: 28.11
            Memory: 11.024 MB
            Memory percentile: 32.56

--- 88. Merge Sorted Array/Solution.cpp
+++ 88. Merge Sorted Array/Solution.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0,  i = m; j < n; j++){
-            nums1[i] = nums2[j];
-            i++;
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int result = m + n - 1;
+        
+        while (ptr2 >= 0) {
+            if (ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]) {
+                nums1[result--] = nums1[ptr1--];
+            } else {
+                nums1[result--] = nums2[ptr2--];
+            }
         }
-        sort(nums1.begin(), nums1.end());
     }
 };
