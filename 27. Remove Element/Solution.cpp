326238c220bad9c5e54c7bd20126768348a0ea0e
Ethan Tarlov <ejtarlov@me.com>
2024-07-04 10:48:53 -0700

    C++ Solution
            Submission Statistics:
            Question #: 27
            Language: C++
            Runtime: 2 ms
            Runtime percentile: 45.42
            Memory: 10.424 MB
            Memory percentile: 44.90

--- 27. Remove Element/Solution.cpp
+++ 27. Remove Element/Solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int index = 0; // Initialize index for placing non-target elements
+        int k = 0; // Initialize index for placing non-target elements
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != val){ // If the current element is not the target value
-                nums[index] = nums[i]; // Place the non-target element at the index position
-                index++;
+                nums[k] = nums[i]; // Place the non-target element at the index position
+                k++;
             }
         }
-        return index;
+        return k;
     }
 };
