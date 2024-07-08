e177999b2e9bb697c98f3392d1bafdbd7fcfb35f
Ethan Tarlov <ejtarlov@me.com>
2024-07-08 16:31:33 -0700

    C++ Solution
            Submission Statistics:
            Question #: 26
            Language: C++
            Runtime: 65 ms
            Runtime percentile: 5.18
            Memory: 20.992 MB
            Memory percentile: 91.22

---
+++ 26. Remove Duplicates from Sorted Array/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    nums.erase(nums.begin()+j);
+                    j--;
+                }
+            }
+        }
+        return nums.size();
+    }
+};
