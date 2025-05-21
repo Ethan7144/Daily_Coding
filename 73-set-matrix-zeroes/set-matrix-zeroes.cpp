b9579cc1e85aa1bd72de6013691dd3606d9abc7d
Ethan Tarlov <ejtarlov@me.com>
2025-05-21 11:39:20 -0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (52.14%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool firstRowHasZero = false;
+        bool firstColHasZero = false;
+
+        for(int j = 0; j < n; j++)
+        {
+            if(matrix[0][j] == 0)
+            {
+                firstRowHasZero = true;
+            }
+        }
+        for(int i = 0; i < m; i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                firstColHasZero = true;
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+         if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
